c2a2bba1c8792198a16c168c1afd613aed65130d
Andreas Näsman <andreas.z.nasman@gmail.com>
2025-02-18 17:19:02 +0200

    Add Android Beacon Library
    https://github.com/AltBeacon/android-beacon-library
    https://altbeacon.github.io/android-beacon-library/index.html

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.android.beacon.library)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
