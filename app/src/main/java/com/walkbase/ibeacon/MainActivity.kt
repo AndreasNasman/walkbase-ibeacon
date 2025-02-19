51b91467f839714c7b4a3a0a5b1056b117ac7af3
Andreas Näsman <andreas.z.nasman@gmail.com>
2025-02-19 17:24:40 +0200

    Add UI template

--- app/src/main/java/com/walkbase/ibeacon/MainActivity.kt
+++ app/src/main/java/com/walkbase/ibeacon/MainActivity.kt
@@ -7,24 +7,40 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.walkbase.ibeacon.sdk.IBeacon
 import com.walkbase.ibeacon.ui.theme.IBeaconTheme
 
 class MainActivity : ComponentActivity() {
+    private var playbackState = mutableStateOf(PlaybackState.STOPPED)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             IBeaconTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DemoApp(
+                        onPauseButtonClick = handlePauseButtonClick,
+                        onPlayButtonClick = handlePlayButtonClick,
+                        onStopButtonClick = handleStopButtonClick,
+                        playbackState = playbackState.value,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -43,6 +59,20 @@ class MainActivity : ComponentActivity() {
                 // Manifest.permission.ACCESS_BACKGROUND_LOCATION,
             )
         )
+    }
+
+    private val handlePauseButtonClick: () -> Unit = {
+        // TODO: Implement
+        playbackState.value = PlaybackState.PAUSED
+    }
+
+    private val handlePlayButtonClick: () -> Unit = {
+        playbackState.value = PlaybackState.PLAYING
+    }
+
+    private val handleStopButtonClick: () -> Unit = {
+        // TODO: Implement
+        playbackState.value = PlaybackState.STOPPED
     }
 
     private val requestPermissionLauncher = registerForActivityResult(
@@ -67,17 +97,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DemoApp(
+    onPauseButtonClick: () -> Unit,
+    onPlayButtonClick: () -> Unit,
+    onStopButtonClick: () -> Unit,
+    playbackState: PlaybackState,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.fillMaxSize()
+    ) {
+        if (playbackState == PlaybackState.STOPPED || playbackState == PlaybackState.PAUSED) {
+            FilledIconButton(onClick = onPlayButtonClick, modifier = Modifier.size(100.dp)) {
+                Icon(
+                    contentDescription = "Play button",
+                    painter = painterResource(R.drawable.play_arrow_24px),
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        } else if (playbackState == PlaybackState.PLAYING) {
+            FilledIconButton(onClick = onPauseButtonClick, modifier = Modifier.size(100.dp)) {
+                Icon(
+                    contentDescription = "Pause button",
+                    painter = painterResource(R.drawable.pause_24px),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(7.dp)
+                )
+            }
+        }
+
+        if (playbackState == PlaybackState.PLAYING || playbackState == PlaybackState.PAUSED) {
+            Spacer(modifier = Modifier.width(10.dp))
+            FilledIconButton(onClick = onStopButtonClick, modifier = Modifier.size(100.dp)) {
+                Icon(
+                    contentDescription = "Stop button",
+                    painter = painterResource(R.drawable.stop_24px),
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IBeaconTheme {
-        Greeting("Android")
+        DemoApp(
+            onPauseButtonClick = {},
+            onPlayButtonClick = {},
+            onStopButtonClick = {},
+            playbackState = PlaybackState.PLAYING
+        )
     }
+}
+
+enum class PlaybackState {
+    STOPPED,
+    PLAYING,
+    PAUSED
 }
