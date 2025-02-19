e8b7164c09aa57353c1fc136575b1fa239b42200
Andreas Näsman <andreas.z.nasman@gmail.com>
2025-02-19 17:19:43 +0200

    Add an IBeacon class with a startBeaconTransmission method
    https://altbeacon.github.io/android-beacon-library/beacon-transmitter.html

---
+++ app/src/main/java/com/walkbase/ibeacon/sdk/IBeacon.kt
@@ -0,0 +1,72 @@
+package com.walkbase.ibeacon.sdk
+
+import android.bluetooth.le.AdvertiseSettings
+import android.content.Context
+import androidx.annotation.IntDef
+import org.altbeacon.beacon.Beacon
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.BeaconTransmitter
+
+// https://www.bluetooth.com/specifications/assigned-numbers/
+private const val APPLE_INC_MANUFACTURER_ID: Int = 0x004C
+
+// https://github.com/AltBeacon/android-beacon-library/issues/13
+private const val IBEACON_LAYOUT = "m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"
+private const val IBEACON_TYPE_CODE: Int = 0x0215
+
+class IBeacon(
+    private val uuid: String,
+    private val beaconLayout: String = IBEACON_LAYOUT,
+    private val beaconTypeCode: Int = IBEACON_TYPE_CODE,
+    private val dataFields: List<Long> = listOf(0L),
+    private val majorValue: String = "1",
+    private val manufacturerId: Int = APPLE_INC_MANUFACTURER_ID,
+    private val minorValue: String = "2",
+    // Signal strength at 1 meter measured in dBm. -59 is a typical starting point for BLE devices.
+    private val txPower: Int = -59,
+) {
+    fun startBeaconTransmission(
+        context: Context,
+        @AdvertiseMode advertiseMode: Int? = null,
+        @AdvertiseTxPowerLevel advertiseTxPowerLevel: Int? = null
+    ) {
+        val beacon = Beacon.Builder()
+            .setId1(uuid)
+            .setId2(majorValue)
+            .setId3(minorValue)
+            .setBeaconTypeCode(beaconTypeCode)
+            .setManufacturer(manufacturerId)
+            .setTxPower(txPower)
+            .setDataFields(dataFields)
+            .build()
+        val beaconParser = BeaconParser().setBeaconLayout(beaconLayout)
+        val beaconTransmitter = BeaconTransmitter(context, beaconParser)
+
+        // Use library defaults if omitted.
+        if (advertiseMode != null) {
+            beaconTransmitter.advertiseMode = advertiseMode
+        }
+        if (advertiseTxPowerLevel != null) {
+            beaconTransmitter.advertiseTxPowerLevel = advertiseTxPowerLevel
+        }
+
+        beaconTransmitter.startAdvertising(beacon)
+    }
+}
+
+@Retention(AnnotationRetention.SOURCE)
+@IntDef(
+    AdvertiseSettings.ADVERTISE_MODE_LOW_POWER,
+    AdvertiseSettings.ADVERTISE_MODE_BALANCED,
+    AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY
+)
+annotation class AdvertiseMode
+
+@Retention(AnnotationRetention.SOURCE)
+@IntDef(
+    AdvertiseSettings.ADVERTISE_TX_POWER_ULTRA_LOW,
+    AdvertiseSettings.ADVERTISE_TX_POWER_LOW,
+    AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM,
+    AdvertiseSettings.ADVERTISE_TX_POWER_HIGH
+)
+annotation class AdvertiseTxPowerLevel
