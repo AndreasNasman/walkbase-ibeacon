19a59079265cab576400c9efcefbe34be6cb3686
Andreas Näsman <andreas.z.nasman@gmail.com>
2025-02-19 11:15:56 +0200

    Handle required permissions
    https://altbeacon.github.io/android-beacon-library/requesting_permission.html
    https://developer.android.com/training/permissions/requesting#allow-system-manage-request-code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.android.beacon.library)
+    implementation(libs.androidx.fragment)
+    implementation(libs.androidx.activity)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
